85202da3578ceae2658bed06873519786bb86a88
LalitaSng <91817854+LalitaSng@users.noreply.github.com>
2023-10-20 15:40:14 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,128 @@
+import { useState } from "react";
+import "./styles.css";
+import { getWinner } from "./util";
+
+export default function App() {
+  const [winner, setWinner] = useState();
+  const [player, setPlayer] = useState("x");
+  const [board, setBoard] = useState([
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""]
+  ]);
+
+  handleClick = (row, col) => {
+    if (board[row][col] != "") return;
+    setBoard((prev) => {
+      const newBoard = [...prev];
+      newBoard[row][col] = player;
+      const winner = getWinner(board, row, col, player);
+      if (winner) setWinner(winner);
+      return newBoard;
+    });
+
+    setPlayer(() => {
+      return player === "x" ? "o" : "x";
+    });
+  };
+
+  function handleButtonClick() {
+    setBoard([
+      ["", "", ""],
+      ["", "", ""],
+      ["", "", ""]
+    ]);
+  }
+
+  return (
+    <div className="App">
+      {!winner && <div> Current Player : {player}</div>}
+      {winner && (
+        <div>
+          Game Over <div> Winner : {winner}</div>
+        </div>
+      )}
+      <div className="row">
+        <div
+          className="column"
+          onClick={() => {
+            handleClick(0, 0);
+          }}
+        >
+          {board[0][0]}
+        </div>
+        <div
+          className="column"
+          onClick={() => {
+            handleClick(0, 1);
+          }}
+        >
+          {board[0][1]}
+        </div>
+        <div
+          className="column"
+          onClick={() => {
+            handleClick(0, 2);
+          }}
+        >
+          {board[0][2]}
+        </div>
+      </div>
+      <div className="row">
+        <div
+          className="column"
+          onClick={() => {
+            handleClick(1, 0);
+          }}
+        >
+          {board[1][0]}
+        </div>
+        <div
+          className="column"
+          onClick={() => {
+            handleClick(1, 1);
+          }}
+        >
+          {board[1][1]}
+        </div>
+        <div
+          className="column"
+          onClick={() => {
+            handleClick(1, 2);
+          }}
+        >
+          {board[1][2]}
+        </div>
+      </div>
+      <div className="row">
+        <div
+          className="column"
+          onClick={() => {
+            handleClick(2, 0);
+          }}
+        >
+          {board[2][0]}
+        </div>
+        <div
+          className="column"
+          onClick={() => {
+            handleClick(2, 1);
+          }}
+        >
+          {board[2][1]}
+        </div>
+        <div
+          className="column"
+          onClick={() => {
+            handleClick(2, 2);
+          }}
+        >
+          {board[2][2]}
+        </div>
+      </div>
+      <button className="button" onClick={handleButtonClick}>
+        Reset
+      </button>
+    </div>
+  );
+}
---
+++ src/util.js
@@ -0,0 +1,52 @@
+export function getWinner(board, row, col, player) {
+  let i,
+    stop = false;
+  for (i = 0; i < board.length; i++) {
+    if (board[row][i] !== player) {
+      stop = true;
+      break;
+    }
+  }
+  if (stop === false) {
+    return player;
+  }
+  stop = false;
+  let j;
+  for (j = 0; j < board.length; j++) {
+    if (board[j][col] !== player) {
+      stop = true;
+      break;
+    }
+  }
+  if (stop == false) {
+    return player;
+  }
+  stop = false;
+  i = 0;
+  j = 0;
+  while (i < board.length && j < board.length) {
+    if (board[i][j] !== player) {
+      stop = true;
+      break;
+    }
+    i++;
+    j++;
+  }
+  if (stop === false) {
+    return player;
+  }
+  stop = false;
+  i = 0;
+  j = board.length - 1;
+  while (i < board.length && j >= 0) {
+    if (board[i][j] !== player) {
+      stop = true;
+      break;
+    }
+    i++;
+    j--;
+  }
+  if (stop === false) {
+    return player;
+  }
+}
